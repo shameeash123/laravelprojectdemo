b0568d4ea8c3dbd5a81bacc2ede1b5c674c104ae
sameera <shameeashraf@gmail.com>
2022-10-12 07:27:22 +0300

    my website file added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div style="border: none  ;width: 1450px;height: 80px ;background-color: rgb(37, 7, 102);">
+<table cellspacing ="15px" cellpadding ="20px">
+    
+   
+    <tr>
+        <th style="background-color:rgb(79, 79, 239) ;"><a href="#id1">HOME</a></th>
+        <th style="background-color:rgb(79, 79, 239) ;"><a href="#id2">ABOUT US</a></th>
+        <th style="background-color:rgb(79, 79, 239) ;"><a href="#id3">IMAGE GALLERY</a></th>
+        <th style="background-color:rgb(79, 79, 239) ;"><a href="#id4">CONTACT US</a></th>
+        
+    </tr>
+</table>
+
+<img src="{{asset('images/1.jpg')}}" alt="">
+<div style="border: 2px solid ;width: 1450px;height: 250px;float: right ;">
+    <h1 id="id1" style="color:BLUE ;font: size 20px;">HOME</h1>
+    <P>
+        ALWAHA CONTRACTING & TRADING COMPANY W.L.L., formerly known as Alwaha Est. for Contracting & Trading is totally a professional company with a team of qualified and dedicated workforce.  Since its inception in 1967, by the Board of Directors – Mr. Ahmed Al Obaidan & Late Mr. Omar R. Abbas, ALWAHA has grown into a Grade ‘A’ Construction Company, inexorably involved in the contemporary progress of Qatar.  Now it is billed as one among the premier engineering construction companies in Qatar. Our faith in ourselves, the courage to face challenges and the sheer determination and perseverance to pursue the goal to its logical conclusion that has enabled ALWAHA to carve out a niche for itself in the highly competitive construction industry.   
+    </P>
+</div>
+<div style="border: 2px solid ;width: 1450px;height: 250px;float: right ;">
+    <h1 id="id2" style="color:BLUE ;font: size 20px;;">About Us</h1>
+    <p>
+        Large reinforced multi-million-gallon capacity concrete reservoirs
+Infrastructure works consisting of sewer and water-mains, telephone and electrical cable installations, irrigation systems
+Construction of Large Pumping Stations for Sewer, Surface Water and Potable Water, together with the associated Electrical and Mechanical Works.
+Industrial developments, diesel power station, transmitting station, compost plant, sludge processing plant
+Residential and commercial buildings, schools, hospitals and exhibition centers
+Electrical & Mechanical installations for industrial, commercial and residential developments
+Assembly of Main Panels, D-B’s, Motor Control Centres, Control Panels, Busbar-trunking
+Civil, Electrical and Mechanical maintenance to Industrial, commercial and residential developments
+Construction of major highways, runways, taxiways and pavements
+Non-Disruptive Construction (Micro Tunnelling), by trained and experienced technicians from Herrenknecht Germany, using the state-of-the-art machines from Messrs. Herreinknectht.
+    </p>
+</div>
+<div border: 2px solid ;width: 1450px;height: 250px;float: right ;>
+    <h1 id="id3" style="color:BLUE ;font: size 20px;;">Image Gallery</h1>
+    <img src="{{asset('images/1.jpg')}}" alt="">
+    <img src="{{asset('images/2.jpg')}}" alt="">
+    <img src="{{asset('images/3.jpg')}}" alt="">
+
+</div>
+<div>
+    <h1 id="id4" style="color:BLUE ;font: size 20px;;">Contact us</h1> 
+    <p>
+        Tele:(+974) 44428167 <br>
+(+974) 44416306 <br>
+Fax:(+974) 44411075 <br>
+P.O.Box:1486 <br>
+Doha – Qatar <br>
+Email: info@alwaha-qatar.com
+    </p>
+</body>
+</html>
